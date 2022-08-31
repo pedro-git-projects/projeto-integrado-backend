a5481ae09006e222ced3a751a0b3a7fa6f5ec568
pedro-git-projects <pedro.generic.email@gmail.com>
2022-08-31 15:45:22 -0300

    fixing typo

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -6,7 +6,7 @@ export const errorMiddleware = (error: HTTPException, req: Request, res: Respons
 		const status: number = error.status || 500;
 		const message: string = error.message || 'Something went wrong';
 
-		console.error(`[${req.method}] ${req.path} >> StatusCode::${status}, Message::${message}`);
+		console.error(`[${req.method}] ${req.path} >> StatusCode::${status} Message::${message}`);
 		res.status(status).json({message});
 	} catch(error) {
 		next(error);
